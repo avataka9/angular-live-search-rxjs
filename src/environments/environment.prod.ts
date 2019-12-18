895844c43832c7515f64ac0041c10dfd808f8af4
Алексей Маруга <alexey.maruga@osinit.com>
2019-12-18 16:56:03 +0300

    WIP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http(s)://5dfa208938678a00145f9d75.mockapi.io/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http(s)://5dfa208938678a00145f9d75.mockapi.io/api/'
 };
 
 /*
